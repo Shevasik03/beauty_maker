cccff2a15a161b9266cd73fcb65e89f6b946295c
Shevasik03 <sanyashev03@gmail.com>
2023-05-25 01:15:50 +0300

    v-25.05.2023

--- blocks/smallCard.php
+++ blocks/smallCard.php
@@ -1,5 +1,21 @@
 <?php
+    $servername = "beauty40_shevasik"; // Назва бази даних
+    $username = "beauty40"; // Ім'я користувача бази даних
+    $password = "f97d62AfKs"; // Пароль бази даних
+    $dbname = "dataBaseProducts"; // Назва бази даних
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Перевірка з'єднання
+    if ($conn->connect_error) {
+        die("Помилка з'єднання з базою даних: " . $conn->connect_error);
+    }
     
+    // Решта коду залишається без змін
+    echo $conn;
+    // Закриття з'єднання із базою даних
+    $stmt->close();
+    $conn->close();
 ?>
     
     
