57a125673b0b36deeeb9708e5221814ab5c062fc
Shevasik03 <sanyashev03@gmail.com>
2023-05-25 06:41:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
-<?php require "blocks/smallCard.php"; ?>
 <?php
     $title = "Головна сторінка";
     require_once "blocks/header.php";
-?>
+    ?>
 
-    <maim class="main">
-
-        <section class="intro">
-            <div class="wrapper">
-                <div class="intro__content flex__sb">
+<maim class="main">
     
-                    <div class="intro__title">
-                        <h2>Український бренд органайзерів </h2>
-                        <p>подаруй незабутні емоції та здійсни маленьку мрію</p>
-                        <button>до каталогу</button>
-                    </div>
+    <section class="intro">
+        <div class="wrapper">
+            <div class="intro__content flex__sb">
+                
+                <div class="intro__title">
+                    <h2>Український бренд органайзерів </h2>
+                    <p>подаруй незабутні емоції та здійсни маленьку мрію</p>
+                    <button>до каталогу</button>
+                </div>
+                
+                <div class="intro__image">
+                    <img src="./img/intro/intro_img1.jpg" alt="">
+                    <img src="./img/intro/intro_img2.jpg" alt="">
+                    <img src="./img/intro/intro_img3.jpg" alt="">
+                    <img src="./img/intro/intro_img4.jpg" alt="">
+                </div>
+                
+            </div> 
+        </div>
+    </section>
     
-                    <div class="intro__image">
-                        <img src="./img/intro/intro_img1.jpg" alt="">
-                        <img src="./img/intro/intro_img2.jpg" alt="">
-                        <img src="./img/intro/intro_img3.jpg" alt="">
-                        <img src="./img/intro/intro_img4.jpg" alt="">
-                    </div>
-    
-                </div> 
-            </div>
-        </section>
-    
+    <?php require "blocks/smallCard.php"; ?>
     
         <?php require "blocks/service.php"; ?>
 
