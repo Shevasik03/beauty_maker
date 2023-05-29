b3220a47fcbba984d48ce042466b6d9f65224af1
Shevasik03 <sanyashev03@gmail.com>
2023-05-29 04:20:49 +0300

    Update footer.php

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,26 +1,26 @@
     <footer class="footer">
         <div class="wrapper">
             <div class="footer__content flex__sb">
-                <div class="footer__left-column">
+                <div class="footer__title">
                     <a href="index.php" class="footer__logo flex__sb">
                         <img src="./img/svg/header_logo.svg" alt="">
                         <h1>BEAUTY MAKER</h1>
                     </a>
-                    <a href="" class="icon__social">
+                    <a href="" class="icon__social flex__sb">
                         <img src="./img/svg/icon_instagram.svg" alt="">
                         <p>instagram</p>
                     </a>
                 </div>
-                <nav class="footer__center-colomn">
+                <nav class="footer__nav">
                     <ul>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
+                        <li><a href="">КАТАЛОГ</a></li>
+                        <li><a href="about_us.php">ПРО НАС</a></li>
+                        <li><a href="#!">ВІДГУКИ</a></li>
+                        <li><a href="faq.php">F.A.Q</a></li>
                     </ul>
                 </nav>
-                <div class="footer__right-column">
-                    <div class="contacts"></div>
+                <div class="footer__contacts">
+                    
                 </div>
             </div>
         </div>
