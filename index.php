23fdfed3a064d6c7d69dc5efa09715e27893b7d8
Shevasik03 <sanyashev03@gmail.com>
2023-05-29 03:45:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -28,8 +28,6 @@
     
     <?php require "blocks/service.php"; ?>
 
-
-
     <section class="bestsellers">
         <div class="wrapper">
             <div class="bestsellers__content">
@@ -48,7 +46,14 @@
     <section class="catalog">
         <div class="wrapper">
             <div class="catalog__content">
-                <div class="catolog__section"></div>
+                <div class="catolog__section">
+                    <div class="card">
+                        <img src="./img/catalog/model_A1.jpg" alt="" class="card__image">
+                        <h4 class="card__title">органайзер <span></span></h4>
+                        <p class="card__size">Розмір: <span></span></p>
+                        <p class="card__price"><span></span> ₴</p>
+                    </div>
+                </div>
             </div>    
         </div>
     </section>
