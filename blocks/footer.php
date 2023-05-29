7aa0039c39abb34babc9944d16d19448768ff794
Shevasik03 <sanyashev03@gmail.com>
2023-05-29 04:30:10 +0300

    footer

--- blocks/footer.php
+++ blocks/footer.php
@@ -20,7 +20,21 @@
                     </ul>
                 </nav>
                 <div class="footer__contacts">
-                    
+                    <h3 class="contacts__title">НАШІ КОНТАКТИ</h3>
+                    <div class="contacts__section">
+                        <div class="contacts__number">
+                            <img src="./img/svg/phone.svg" alt="">
+                            <p>+380 99 666 66 66</p>
+                        </div>
+                        <div class="contacts__location">
+                            <img src="./img/svg/location.svg" alt="">
+                            <p>Київ, Україна</p>
+                        </div>
+                        <div class="contacts__email">
+                            <img src="./img/svg/email.svg" alt="">
+                            <p>beautymake@ukr.net</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
